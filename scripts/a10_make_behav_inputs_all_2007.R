b6f438cabc7b1f3010820ed59764f0933d891ea0
Steven M. Goodreau <goodreau@uw.edu>
2020-05-28 15:56:21 -0700

    update a10_make_behav_inputs_all_2007

--- scripts/a10_make_behav_inputs_all_2007.R
+++ scripts/a10_make_behav_inputs_all_2007.R
@@ -2,11 +2,11 @@
 ### Put the actual dynamic numbers separate objects so they can be recalled later
 
 pred_eversex_f_dyn <- pred_eversex_f
-pred_eversex_m_dyn <- pred_eversex_m
+#pred_eversex_m_dyn <- pred_eversex_m
 pred_condom_f_dyn <- pred_condom_f
-pred_condom_m_dyn <- pred_condom_m
+#pred_condom_m_dyn <- pred_condom_m
 pred_mnppy_f_dyn <- pred_mnppy_f
-pred_mnppy_m_dyn <- pred_mnppy_m
+#pred_mnppy_m_dyn <- pred_mnppy_m
 
 #########################################################################
 ### Change all post-2007 numbers to match 2007 (ie as if no behavior change)
@@ -18,10 +18,10 @@ pred_mnppy_m_dyn <- pred_mnppy_m
 
 
 for (i in 2:dim(pred_eversex_f)[3]) pred_eversex_f[,,i] <- pred_eversex_f[,,1]
-for (i in 2:dim(pred_eversex_m)[3]) pred_eversex_m[,,i] <- pred_eversex_m[,,1]
+#for (i in 2:dim(pred_eversex_m)[3]) pred_eversex_m[,,i] <- pred_eversex_m[,,1]
 for (i in 2:dim(pred_condom_f)[3]) pred_condom_f[,,i] <- pred_condom_f[,,1]
-for (i in 2:dim(pred_condom_m)[3]) pred_condom_m[,,i] <- pred_condom_m[,,1]
+#for (i in 2:dim(pred_condom_m)[3]) pred_condom_m[,,i] <- pred_condom_m[,,1]
 for (i in 2:dim(pred_mnppy_f)[3]) pred_mnppy_f[,,i] <- pred_mnppy_f[,,1]
-for (i in 2:dim(pred_mnppy_m)[3]) pred_mnppy_m[,,i] <- pred_mnppy_m[,,1]
+#for (i in 2:dim(pred_mnppy_m)[3]) pred_mnppy_m[,,i] <- pred_mnppy_m[,,1]
 
 save.image("../output/a10_inputs_all_2007.rda")
