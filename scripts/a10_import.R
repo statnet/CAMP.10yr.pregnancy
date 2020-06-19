8d848edd5b81cb605bac558b21313fc5496b8508
Steven M. Goodreau <goodreau@uw.edu>
2020-06-19 15:58:27 -0700

    coding up birth control importing

--- scripts/a10_import.R
+++ scripts/a10_import.R
@@ -14,11 +14,16 @@ years <- seq(2007, 2017, by=2)
 ages <- 13:18
 eths <- c("Black", "Hispanic", "White")
 eths_all <- c("Black", "Hispanic", "White", "Other")  # For popsizes, to remove Others from census estimates
+eths_lc <- c("black", "hispanic", "white")
+
+bctypes <- c("no method", "condoms", "hormonal", "hormonal+LARC", "LARC", "withdrawal", 
+             "other79", "other1", "other357")
 
 nages <- length(ages)
 nyears <- length(years)
 neths <- length(eths)
 neths_all <- length(eths_all)
+nbctypes <- length(bctypes)
 
 ##### Read in the pop size weights
 wts_f <- wts_m <- array(dim=c(neths_all, nages, nyears))
@@ -60,50 +65,79 @@ for (i in 1:length(years)) {
 eversex_f <- eversex_f %>% replace_na(0)
 eversex_m <- eversex_m %>% replace_na(0)
 
-### Read in the condom numbers
-## Condoms are expressed differently than previous values, as popsizes for no and yes separately
+### Read in the bith control numbers
+## These are expressed differently than previous values, as popsizes for each type
+## And the types keep changing across years
+## There are a total of nine different possible repsonses with different associated 
+##    efficacies present in at least 1 year:
+##       - no method:     2007, 2009, 2011, 2013, 2015, 2017
+##       - condoms:       2007, 2009, 2011, 2013, 2015, 2017
+##       - hormonal:      2007, 2009, ----, 2013, 2015, 2017
+##       - hormonal+LARC: ----, ----, 2011, ----, ----, ----
+##       - LARC:          ----, ----, ----, 2013, 2015, 2017
+##       - withdrawal:    2007, 2009, 2011, ----, ----, ----
+##       - other79:       2007, 2009, ----, ----, ----, ----
+##       - other1:        ----, ----, 2011, ----, ----, ----
+##       - other357:      ----, ----, ----, 2013, 2015, 2017
+##
 
-condom_f <- condom_m <- array(dim=c(neths, nages, nyears))
-condom_wts_f <- condom_wts_m <- array(dim=c(neths, nages, nyears))
+## "no method", "condoms", "hormonal", "hormonal+LARC", "LARC", "withdrawal", 
+##             "other79", "other1", "other357"
+             
+## Note that names of the three "others" are my names, meant to distinguish them in terms of what
+##   they represent ("other" relative to three different sets of provided lists)
+##  The names for them in the actual data files are:
+##    other79 =  "other"
+##    other1  =  "other"
+##    other357 = "withdrawal/other"
+##  The code below not only imports the nine types, but records the various others to these 
+##    three named types of other
+
+#condom_f <- condom_m <- array(dim=c(neths, nages, nyears))
+#condom_wts_f <- condom_wts_m <- array(dim=c(neths, nages, nyears))
+
+bctype_wts <- array(dim=c(neths, nages, nyears, nbctypes))
 
 for (i in 1:length(years)) {
-  filename <- paste(datapath, "/condom_", years[i], ".txt", sep="")
+  filename <- paste(datapath, "/bctypes_", years[i], ".csv", sep="")
   temp <- read.csv(filename)
-  temp$freq <- temp$freq %>% replace_na(0)
+  temp$WgtFreq <- suppressWarnings(as.numeric(as.character(temp$WgtFreq)))
+  temp$WgtFreq <- temp$WgtFreq %>% replace_na(0)
+
+  if(years[i] %in% c(2007, 2009))       temp$pregprev <- recode(temp$pregprev, other="other79")
+  if(years[i] %in% c(2011))             temp$pregprev <- recode(temp$pregprev, other="other1")
+  if(years[i] %in% c(2013, 2015, 2017)) temp$pregprev <- recode(temp$pregprev, 
+                                                                "withdrawal/other"="other357")
+  
   for (j in 1:neths) {
     for (k in 1:nages) {
-      if(nrow(temp %>% filter(sex_active=="Female", race==eths[j], age==ages[k]))==0) {  ## Cases where row is missing altogether
-         condom_f[j,k,i] <- 0
-         condom_wts_f[j,k,i] <- 0
-      } else {
-        if(sum(temp %>% filter(sex_active=="Female", race==eths[j], age==ages[k]) %>% dplyr::select(freq))==0) {  ## Cases where freq is 0 (either in the original data, or as a replacement for NA as done above)
-          condom_f[j,k,i] <- 0
-          condom_wts_f[j,k,i] <- 0
-        } else {
-          condom_f[j,k,i] <- unname(unlist(
-            temp %>% filter(sex_active=="Female", race==eths[j], age==ages[k], condomuse=="Yes") %>% dplyr::select(freq) / 
-              sum(temp %>% filter(sex_active=="Female", race==eths[j], age==ages[k]) %>% dplyr::select(freq))
-            ))
-          condom_wts_f[j,k,i] <- sum(temp %>% filter(sex_active=="Female", race==eths[j], age==ages[k]) %>% dplyr::select(freq))
-      }}
-      if(nrow(temp %>% filter(sex_active=="Male", race==eths[j], age==ages[k]))==0) {  ## Cases where row is missing altogether
-        condom_m[j,k,i] <- 0
-        condom_wts_m[j,k,i] <- 0
-      } else {
-        if(sum(temp %>% filter(sex_active=="Male", race==eths[j], age==ages[k]) %>% dplyr::select(freq))==0) {  ## Cases where freq is 0 (either in the original data, or as a replacement for NA as done above)
-          condom_m[j,k,i] <- 0
-          condom_wts_m[j,k,i] <- 0
-        } else {
-          condom_m[j,k,i] <- unname(unlist(
-            temp %>% filter(sex_active=="Male", race==eths[j], age==ages[k], condomuse=="Yes") %>% dplyr::select(freq) / 
-              sum(temp %>% filter(sex_active=="Male", race==eths[j], age==ages[k]) %>% dplyr::select(freq))
-          ))
-          condom_wts_m[j,k,i] <- sum(temp %>% filter(sex_active=="Male", race==eths[j], age==ages[k]) %>% dplyr::select(freq))
-      }}
+      for (m in 1:nbctypes) {
+          if(nrow(temp %>% filter(sex=="female", race==eths_lc[j], age==ages[k], pregprev==bctypes[m]
+                                  ))==0) {  ## Cases where row is missing altogether
+             bctype_wts[j,k,i,m] <- 0
+          } else {
+            if(sum(temp %>% filter(sex=="female", race==eths_lc[j], age==ages[k], pregprev==bctypes[m]) %>% dplyr::select(WgtFreq))==0) {  
+                      ## Cases where freq is 0 (either in the original data, or as a replacement for NA as done above)
+              bctype_wts[j,k,i,m] <- 0
+            } else {
+              bctype_wts[j,k,i,m] <- unlist(temp %>% filter(sex=="female", race==eths_lc[j], age==ages[k], pregprev==bctypes[m]) %>% dplyr::select(WgtFreq))
+            }}  
+      }
     }
   }
 }
 
+bctype_prob <- sweep(bctype_wts, 1:3, apply(bctype_wts, 1:3, sum), "/")
+bctype_yearprob <- apply(bctype_wts, c(3,4), sum) / apply(bctype_wts, 3, sum)
+bctype_yearprob <- na_if(bctype_yearprob, 0)
+matplot(bctype_yearprob, type='b', xaxt="n" , ylab= "Prop. reporting method",
+        main = "Method of birth control reported be females, YRBS", ylim=c(0,0.7))
+axis(1, 1:6, seq(2007, 2017, 2))
+legend(2, 0.7, c(
+  '1 = no method', '2 = condoms', '3 = hormonal', '4 = hormonal or LARC', 
+  '5 = LARC', '6 = withdrawal', '7 = other (including LARC)', '8 = other',
+  '9 = other (including withdrawal)'),
+  cex=0.7, text.col=1:6, col=1:6, lty= 1:5, ncol=2)
 
 ### Read in matrix1 (number by race by current age by age of debut by year)
 ## notice stop-gap in terms of dim 3 size
