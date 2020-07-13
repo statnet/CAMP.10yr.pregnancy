370f0dc4f1d6e8d23af9b2d173f9494560201c4e
Steven M. Goodreau <goodreau@uw.edu>
2020-07-13 14:33:04 -0700

    added script to impute even years

---
+++ scripts/a10_impute_even_years.R
@@ -0,0 +1,22 @@
+
+#########################################################################
+# teen-SPARC 10-year pregnancy paper - reassign bc types
+# 
+# Notes: 
+#   1. Change the setwd() line if needed
+
+#########################################################################
+### Basics
+#setwd("C:/git/CAMP_10yr_pregnancy/scripts/")
+
+# Create object 
+for (year in c(2,4,6,8,10)) {
+  for (method in 1:6) {
+    pred_bctype_minLARC[[method]][,,year] <- (pred_bctype_minLARC[[method]][,,year-1] + 
+                                              pred_bctype_minLARC[[method]][,,year+1]) /
+                                              2
+  }
+}
+
+
+
