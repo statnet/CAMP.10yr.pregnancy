34fbd81b4ea80e2763ec3f76a3fb4e58c91eaa41
Steven M. Goodreau <goodreau@uw.edu>
2020-07-13 14:33:17 -0700

    added script to reassign bctypes

---
+++ scripts/a10_reassign_bctypes.R
@@ -0,0 +1,84 @@
+
+#########################################################################
+# teen-SPARC 10-year pregnancy paper - reassign bc types
+# 
+# Notes: 
+#   1. Change the setwd() line if needed
+
+#########################################################################
+### Basics
+#setwd("C:/git/CAMP_10yr_pregnancy/scripts/")
+
+#Decide which model to use
+pred_bctype_in <- pred_bctype_agefac
+
+# Create object 
+pred_bctype <- list(no_method = array(dim=c(3,6,11)),
+                    condoms             = array(dim=c(3,6,11)),
+                    pills               = array(dim=c(3,6,11)),
+                    LARC                = array(dim=c(3,6,11)),
+                    other_hormonal      = array(dim=c(3,6,11)),
+                    withdrawal_other    = array(dim=c(3,6,11))
+)
+
+
+# Reassign the ones that are straightforward across all years
+pred_bctype$no_method <- pred_bctype_in$no_method
+pred_bctype$condoms <- pred_bctype_in$condoms
+pred_bctype$pills <- pred_bctype_in$pills
+
+# Reassign the ones that are straightforward for some years
+
+# 2007, 2009
+for (year in c(1,3)) {
+  pred_bctype$other_hormonal[,,year] <- pred_bctype_in$injection[,,year]
+  pred_bctype$withdrawal_other[,,year] <- pred_bctype_in$withdrawal[,,year]
+}
+
+# 2011
+for (year in c(5)) {
+  pred_bctype$withdrawal_other[,,year] <- pred_bctype_in$other1[,,year] +
+                                          pred_bctype_in$withdrawal[,,year]
+}
+
+# 2013, 2015, 2017
+for (year in c(7,9,11)) {
+  pred_bctype$LARC[,,year] <- pred_bctype_in$LARC[,,year]
+  pred_bctype$other_hormonal[,,year] <- pred_bctype_in$other_hormonal[,,year]
+  pred_bctype$withdrawal_other[,,year] <- pred_bctype_in$withdrawal_other[,,year]
+}
+
+##### Set up different models based on assumptions
+
+###### minimum LARC ###
+
+pred_bctype_minLARC <- pred_bctype
+
+for (year in c(1,3,5)) {
+  pred_bctype_minLARC$LARC[,,year] <- 0
+}
+
+for (year in c(1,3)) {
+  pred_bctype_minLARC$other_hormonal[,,year] <- pred_bctype$other_hormonal[,,year] + pred_bctype_in$other79[,,year]
+}
+
+for (year in c(5)) {
+  pred_bctype_minLARC$other_hormonal[,,year] <- pred_bctype_in$other_hormonal_LARC[,,year]
+}
+
+# Check to see it's all 1s
+temp <- pred_bctype_minLARC[[1]] + pred_bctype_minLARC[[2]] + pred_bctype_minLARC[[3]] + 
+  pred_bctype_minLARC[[4]] + pred_bctype_minLARC[[5]] + pred_bctype_minLARC[[6]]
+temp <- round(temp,5)  
+sum(temp[,,c(1,3,5,7,9,11)]!=1)  # should equal 0
+
+
+#pred_bctype_maxLARC <- pred_bctype
+
+
+
+#######################################################################
+### NOTES
+
+save.image("../output/a10_reassigned_bctypes.rda")
+
