23d1b8a3a1b22aa82c30492e6d40af2b9c16717f
Steven M. Goodreau <goodreau@uw.edu>
2020-07-20 14:52:09 -0700

    rewriting "transmission function" to pregnancy function and restructuring in terms of total pop

--- R/a10.R
+++ R/a10.R
@@ -3,14 +3,15 @@
 #'Main a10 function
 #'
 #' @param n_f A 3x6x11 array indicating the population size for females, by race/eth by age by year
-#' @param prop_eversex_f A 3x6x11 matrix inidicating the proportion of females debuted (as FSM), by race/eth by age by year
-#' @param condom_use_f, A 3x6x11 matrix indicating % condom use for females, by race/eth by age by year
+#' @param prop_eversex_f A 3x6x11 matrix indicating the proportion of females debuted (as FSM), by race/eth by age by year
+#' @param bc_use_f, A list of 6 3x6x11 matrices indicating birth control use for females, by race/eth by age by year
 #' @param mean_new_part_f A 3x6x11 matrix indicating the mean new partners per year for debuted females, by race/eth by age
 #' @param coital_acts_pp_f A 3x6x11 matrix indicating the mean coital acts per partner for females, by race/eth by age
 #' @param preg_init_f A vector of length 3 or a 3x6 matrix, indicating the number of recent annual pregnancies among females, by race/eth (and optionally by age) -- in and out of school
 #' @param prob_detpreg_f Per-act transmission probability from male to female
 #' @param meanpop_tot_f Total female population in and out of school across the relevant ages
-#'
+#' @param failure_rate A list of 6 3x6 matrices indicating the relative failure rate of the six different BC methods, by race/eth by age 
+#' '
 #' @return A list comprising an array of dimensions [3,6,12], containing the estimated number of
 #'   pregnancies per age per year. The three rows represent the
 #'   three race/ethnicity groups (B, H, W); the six columns represent the ages (13:18);
@@ -19,12 +20,13 @@
 #' @export
 a10_preg <- function(n_f,
                 prop_eversex_f,
-                condom_use_f,
+                bc_use_f,
                 mean_new_part_f,
                 coital_acts_pp_f,
                 preg_init_f,
                 prob_detpreg_f,
-                meanpop_tot_f
+                meanpop_tot_f,
+                failure_rate
         ) {
 
   ##################################################
@@ -39,7 +41,8 @@ a10_preg <- function(n_f,
 
   # Calc # who have sexually debuted
   n_eversex_f <- n_f * prop_eversex_f
-
+  n_eversex_f_tot <- meanpop_tot_f * prop_eversex_f
+  
   # Create arrays to store number of pregnancies per year **in school** and *total*
   n_preg_insch_f <- n_preg_total_f <- array(dim=c(3,6,12))
   n_preg_insch_f[,,1] <- n_preg_total_f[,,1] <- NA
@@ -83,9 +86,10 @@ a10_preg <- function(n_f,
   #}
 
   # Calculate the number of condomless acts per person
-  cl_acts_f <- mean_new_part_f * coital_acts_pp_f * (1-condom_use_f)
+  #cl_acts_f <- mean_new_part_f * coital_acts_pp_f * (1-condom_use_f)
   #cl_acts_m <- mean_new_part_m * coital_acts_pp_m * (1-condom_use_m)
-
+  coital_acts_f <- mean_new_part_f * coital_acts_pp_f
+  
   ##########################################################################
   # Advancement
 
@@ -98,13 +102,23 @@ a10_preg <- function(n_f,
     #overall_prev_f <- rowSums(prev_f[,,i-1]*meanpop_tot_f[,,i-1]) /   
     #                            rowSums(meanpop_tot_f[,,i-1])
 
-    n_preg_insch_f[,,i] <- (n_eversex_f[,,i-1]) * (1-(1-prob_detpreg_f[,])^(cl_acts_f[,,i-1]))
+    n_preg_total_f[,,i] <- 
+      (n_eversex_f_tot[,,i-1]) * 
+        (1-
+          (1-prob_detpreg_f[,]*(failure_rate[[1]]))^(coital_acts_f[,,i-1]*bc_use_f[[1]][,,i-1]) * 
+          (1-prob_detpreg_f[,]*(failure_rate[[2]]))^(coital_acts_f[,,i-1]*bc_use_f[[2]][,,i-1]) *
+          (1-prob_detpreg_f[,]*(failure_rate[[3]]))^(coital_acts_f[,,i-1]*bc_use_f[[3]][,,i-1]) *
+          (1-prob_detpreg_f[,]*(failure_rate[[4]]))^(coital_acts_f[,,i-1]*bc_use_f[[4]][,,i-1]) *
+          (1-prob_detpreg_f[,]*(failure_rate[[5]]))^(coital_acts_f[,,i-1]*bc_use_f[[5]][,,i-1]) *
+          (1-prob_detpreg_f[,]*(failure_rate[[6]]))^(coital_acts_f[,,i-1]*bc_use_f[[6]][,,i-1])
+      )
+
+    n_preg_insch_f[,,i] <- n_preg_total_f[,,i] * n_f[,,1] / meanpop_tot_f[,,1] 
+    
+    #n_preg_insch_f[,,i] <- (n_eversex_f[,,i-1]) * (1-(1-prob_detpreg_f[,])^(cl_acts_f[,,i-1]))
     #n_diag_insch_f[,,i] <- n_inc_insch_f[,,i] * prop_diag_f
-    n_preg_total_f[,,i] <- n_preg_insch_f[,,i] * meanpop_tot_f[,,1] / n_f[,,1]
     #n_diag_total_f[,,i] <- n_diag_insch_f[,,i] * meanpop_tot_f[,,1] / n_f[,,1]
-
     #prev_f[,,i] <- n_inc_insch_f[,,i]*dur_inf_f / n_f[,,i-1]                        
-
   }
 
   ##########################################################################
@@ -115,7 +129,8 @@ a10_preg <- function(n_f,
                  #n_diag_insch_f = n_diag_insch_f,
                  #n_diag_total_f = n_diag_total_f,
                  n_eversex_f = n_eversex_f,
-                 cl_acts_f = cl_acts_f
+                 n_eversex_f_tot = n_eversex_f_tot
   )
   return(result)
 }
+
