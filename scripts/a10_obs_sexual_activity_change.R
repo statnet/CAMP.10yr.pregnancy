0e177804e49114c8e3b38bcca68b082fc8edaa5a
Steven M. Goodreau <goodreau@uw.edu>
2020-10-01 09:21:22 -0700

    add obs sexual activity script

---
+++ scripts/a10_obs_sexual_activity_change.R
@@ -0,0 +1,20 @@
+
+#########################################################################
+### Call main function
+
+a10_minLARC_obs_sex <- a10_preg(n_f = n_f, 
+                             prop_eversex_f = pred_eversex_f_dyn,
+                             bc_use = pred_bctype_minLARC,
+                             mean_new_part_f = pred_mnppy_f_dyn,
+                             coital_acts_pp_f = capp_f,
+                             preg_init_f = preg_init_f,             
+                             prob_detpreg_f = prob_detpreg,        
+                             meanpop_tot_f = meanpop_13to18_f,
+                             failure_rate = failure_rate
+  )
+
+#########################################################################
+### Process results
+
+save(a10_minLARC_obs_sex, file='../output/a10_minLARC_obs_sex.rda')
+
