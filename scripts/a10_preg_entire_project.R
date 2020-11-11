a80fa18eb443885f717165ce102e9243f774a396
Steven M. Goodreau <goodreau@uw.edu>
2020-11-11 12:52:14 -0800

    updating preg_entire_project

--- scripts/a10_preg_entire_project.R
+++ scripts/a10_preg_entire_project.R
@@ -21,7 +21,8 @@ set.seed(1)
 
 source("a10_preg_backcalc_logistic.R")
 source("a10_import.R")                          # Get all inputs
-source("a10_process_inputs.R")                  # Process inputs (i.e. conduct regressions, etc.)
+source("a10_process_inputs_all_but_bctypes.R")  # Process inputs (i.e. conduct regressions, etc.)
+source("a10_process_inputs_bctypes.R")          # Process inputs (i.e. conduct regressions, etc.)
 source("a10_reassign_bctypes.R")                # Move bc methods from input types to standardized types
 source("a10_impute_even_years.R")               # Impute even years
 source("a10_make_behav_inputs_all_2007.R")      # override 2009-2017 numbers with 2007 for both calibration and no-behavior-change models
@@ -37,12 +38,10 @@ source("a10_obs_sexual_activity_change_minLARC.R")   # Observed debut / partner 
 source("a10_obs_debut_change_minLARC.R")             # Observed debut only
 source("a10_obs_mnppy_change_minLARC.R")             # Observed partner numbers only
 
-#source("a10_credible_intervals.R")           # Observed behavior change
-
 ########################################################################
 ### # Scenario 2: Maximum LARC possible given responses from 2009 on (maxLARC)
 
-source("a10_ABC_maxLARC.R")
+#source("a10_ABC_maxLARC.R")
 source("a10_calibration_maxLARC.R")                  # calib pt 1
 source("a10_no_behav_change_maxLARC.R")              # No behavior change
 source("a10_obs_behav_change_maxLARC.R")             # Observed behavior change
@@ -50,6 +49,11 @@ source("a10_obs_contraception_change_maxLARC.R")     # Observed contraception ch
 source("a10_obs_sexual_activity_change_maxLARC.R")   # Observed debut / partner numbers only
 source("a10_obs_debut_change_maxLARC.R")             # Observed debut only
 source("a10_obs_mnppy_change_maxLARC.R")             # Observed partner numbers only
+
+########################################################################
+#source("a10_credible_intervals.R")           # Observed behavior change
+
+
 
 
 ########################################################
