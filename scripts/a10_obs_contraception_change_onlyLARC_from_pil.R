e31ae038df83b7297f31e416372a2831b992872e
Steven M. Goodreau <goodreau@uw.edu>
2021-09-03 17:52:57 -0700

    Add onlyLARC scenarios from pills and from all sources

---
+++ scripts/a10_obs_contraception_change_onlyLARC_from_pil.R
@@ -0,0 +1,35 @@
+#########################################################################
+### Call main function
+
+a10_obs_cc_onlyLARC_minL_from_pil <- a10_preg(n_f = n_f, 
+                                      prop_eversex_f = pred_eversex_f,
+                                      bc_use = pred_bctype_onlyLARC_minL_from_pil,
+                                      mean_new_part_f = pred_mnppy_f,
+                                      coital_acts_pp_f = capp_f,
+                                      preg_init_f = preg_init_f,             
+                                      prob_detpreg_f = prob_detpreg_minLARC,        
+                                      meanpop_tot_f = meanpop_13to18_f,
+                                      failure_rate = failure_rate
+)
+
+a10_obs_cc_onlyLARC_maxL_from_pil <- a10_preg(n_f = n_f, 
+                                      prop_eversex_f = pred_eversex_f,
+                                      bc_use = pred_bctype_onlyLARC_maxL_from_pil,
+                                      mean_new_part_f = pred_mnppy_f,
+                                      coital_acts_pp_f = capp_f,
+                                      preg_init_f = preg_init_f,             
+                                      prob_detpreg_f = prob_detpreg_maxLARC,        
+                                      meanpop_tot_f = meanpop_13to18_f,
+                                      failure_rate = failure_rate
+)
+
+a10_obs_cc_onlyLARC_medL_from_pil <- a10_preg(n_f = n_f, 
+                                      prop_eversex_f = pred_eversex_f,
+                                      bc_use = pred_bctype_onlyLARC_medL_from_pil,
+                                      mean_new_part_f = pred_mnppy_f,
+                                      coital_acts_pp_f = capp_f,
+                                      preg_init_f = preg_init_f,             
+                                      prob_detpreg_f = prob_detpreg_medLARC,        
+                                      meanpop_tot_f = meanpop_13to18_f,
+                                      failure_rate = failure_rate
+)
