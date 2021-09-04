323f8d7633468507d94b402e3887c7dc79d9be10
Steven M. Goodreau <goodreau@uw.edu>
2021-09-03 17:52:35 -0700

    Add medLARC to onlyLARC scenarios from withdrawal and condoms

--- scripts/a10_obs_contraception_change_onlyLARC_from_cdm.R
+++ scripts/a10_obs_contraception_change_onlyLARC_from_cdm.R
@@ -22,3 +22,14 @@ a10_obs_cc_onlyLARC_maxL_from_cdm <- a10_preg(n_f = n_f,
                                       meanpop_tot_f = meanpop_13to18_f,
                                       failure_rate = failure_rate
 )
+
+a10_obs_cc_onlyLARC_medL_from_cdm <- a10_preg(n_f = n_f, 
+                                      prop_eversex_f = pred_eversex_f,
+                                      bc_use = pred_bctype_onlyLARC_medL_from_cdm,
+                                      mean_new_part_f = pred_mnppy_f,
+                                      coital_acts_pp_f = capp_f,
+                                      preg_init_f = preg_init_f,             
+                                      prob_detpreg_f = prob_detpreg_medLARC,        
+                                      meanpop_tot_f = meanpop_13to18_f,
+                                      failure_rate = failure_rate
+)
