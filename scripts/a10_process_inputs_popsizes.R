74342564c260f0a5dcfd70d04f5fff2a79f7a691
Steven M. Goodreau <goodreau@uw.edu>
2022-06-10 10:47:24 -0700

    adding in processing of m pop sizes for descriptives

--- scripts/a10_process_inputs_popsizes.R
+++ scripts/a10_process_inputs_popsizes.R
@@ -1,8 +1,8 @@
 
 #########################################################################
 # teen-SPARC 10-year pregnancy paper - input processing
-# 
-# Notes: 
+#
+# Notes:
 #   1. Change the setwd() line if needed
 
 #########################################################################
@@ -16,26 +16,27 @@ nyears <- length(years)
 ### HS-attending pop sizes by age/race/sex (averaged across years)
 
 meanschoolpop <- mean(schoolpops$totschoolpop)             # Tot pop size from census, avgd across years
-#mean_prop_age_race <- apply(wts_f+wts_m, 1:2, mean) / 
+#mean_prop_age_race <- apply(wts_f+wts_m, 1:2, mean) /
 #          sum(apply(wts_f+wts_m, 1:2, mean))               # Tot % age/race from YRBS, avgd across years and sexes
-mean_prop_age_race <- apply(wts_f, 1:2, mean) / 
+mean_prop_age_race <- apply(wts_f, 1:2, mean) /
   sum(apply(wts_f, 1:2, mean))               # Tot % age/race from YRBS, avgd across years and sexes
 
 prop_f <- sum(wts_f) / (sum(wts_f) + sum(wts_m))            # Tot % female from YRBS, avgd across years
 
 n_f <- array11(mean_prop_age_race * prop_f * meanschoolpop)
-#n_m <- array11(mean_prop_age_race * (1-prop_f) * meanschoolpop)
+n_m <- array11(mean_prop_age_race * (1-prop_f) * meanschoolpop)
 
 n_f <- n_f[-4,,]
-#n_m <- n_m[-4,,]
+n_m <- n_m[-4,,]
+
 wts_f <- wts_f[-4,,]
-#wts_m <- wts_m[-4,,]
+wts_m <- wts_m[-4,,]
 
 #########################################################################
 ### Total (HS or not HS) pop sizes by age/race/sex (averaged across years)
 
 #totpop <- totpop_f + totpop_m
-totpop <- totpop_f 
+totpop <- totpop_f
 meanpop_13to18 <- apply(totpop, 1:2, mean, na.rm=TRUE)
 
 #totpop_prop_f <- sum(totpop_f, na.rm=TRUE) / (sum(totpop_f, na.rm=TRUE) + sum(totpop_m, na.rm=TRUE))
